63d370888ebccc9d8de7e1c614ec86b4fff7279a
Thibb1 <thibault.brumaire@gmail.com>
2023-01-09 13:30:16 +0100

    [+] first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** main.cpp by thibb1
+*/
+
+#include <iostream>
+
+int main(int ac, char *av[]) {
+    std::cout << "hello world !" << std::endl;
+    return 0;
+}
