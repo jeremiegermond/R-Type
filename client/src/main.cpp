bbb4c87e3a9b5c7b4dbac41cdd275618cbea69cd
Thibb1 <thibault.brumaire@gmail.com>
2023-01-11 14:41:48 +0100

    [+] switched ball to cube

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -10,30 +10,37 @@
 int screenWidth = 800;
 int screenHeight = 450;
 
-void UpdateDrawFrame(Vector2 *ballPosition) {
-    float speed = 10.0f;
-    if (IsKeyDown(KEY_RIGHT))
-        ballPosition->x += speed;
-    if (IsKeyDown(KEY_LEFT))
-        ballPosition->x -= speed;
-    if (IsKeyDown(KEY_UP))
-        ballPosition->y -= speed;
-    if (IsKeyDown(KEY_DOWN))
-        ballPosition->y += speed;
+void UpdateDrawFrame(Vector3 *cubePosition, Camera3D camera) {
+    float speed = 1.0f;
+    if (IsKeyPressed(KEY_RIGHT))
+        cubePosition->x += speed;
+    if (IsKeyPressed(KEY_LEFT))
+        cubePosition->x -= speed;
+    if (IsKeyPressed(KEY_UP))
+        cubePosition->z -= speed;
+    if (IsKeyPressed(KEY_DOWN))
+        cubePosition->z += speed;
     BeginDrawing();
+    BeginMode3D(camera);
     ClearBackground(BLACK);
-    DrawGrid(10, 10.0f);
-    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-    DrawCircleV(*ballPosition, 50, MAROON);
+    DrawCube(*cubePosition, 1, 1, 1, RED);
+    DrawGrid(10, 1.0f);
+    EndMode3D();
     EndDrawing();
 }
 
 int main(int ac, char *av[]) {
     InitWindow(screenWidth, screenHeight, "it rayworks !");
     SetTargetFPS(60);
-    Vector2 ballPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
+    Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
+    Camera3D camera = {0};
+    camera.position = (Vector3){0.0f, 1.0f, 10.0f}; // Camera position
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};    // Camera looking at point
+    camera.up = (Vector3){0.0f, 1.0f, 0.0f};        // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                            // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;
     while (!WindowShouldClose()) {
-        UpdateDrawFrame(&ballPosition);
+        UpdateDrawFrame(&cubePosition, camera);
     }
     CloseWindow();
     return 0;
