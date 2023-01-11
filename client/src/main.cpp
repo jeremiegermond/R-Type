838feee9efa32dbc270e352cd1f1b7c1b4d1dad0
Thibb1 <thibault.brumaire@gmail.com>
2023-01-11 13:31:18 +0100

    [+] ball with movement

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -10,18 +10,30 @@
 int screenWidth = 800;
 int screenHeight = 450;
 
-void UpdateDrawFrame() {
+void UpdateDrawFrame(Vector2 *ballPosition) {
+    float speed = 10.0f;
+    if (IsKeyDown(KEY_RIGHT))
+        ballPosition->x += speed;
+    if (IsKeyDown(KEY_LEFT))
+        ballPosition->x -= speed;
+    if (IsKeyDown(KEY_UP))
+        ballPosition->y -= speed;
+    if (IsKeyDown(KEY_DOWN))
+        ballPosition->y += speed;
     BeginDrawing();
-    ClearBackground(RAYWHITE);
+    ClearBackground(BLACK);
+    DrawGrid(10, 10.0f);
     DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    DrawCircleV(*ballPosition, 50, MAROON);
     EndDrawing();
 }
 
 int main(int ac, char *av[]) {
     InitWindow(screenWidth, screenHeight, "it rayworks !");
     SetTargetFPS(60);
+    Vector2 ballPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
     while (!WindowShouldClose()) {
-        UpdateDrawFrame();
+        UpdateDrawFrame(&ballPosition);
     }
     CloseWindow();
     return 0;
