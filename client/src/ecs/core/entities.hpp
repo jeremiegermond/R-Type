3239690790e47c5ec0223e13f5122e56693a9eb8
Jérémie Germond <jeremie.germond@epitech.eu>
2023-01-13 15:34:38 +0100

    [+] adding the beginning of the ECS, with a position and a velocity and draws a window, fixed cmakelist to compile on windows and create an executable for the client

---
+++ client/src/ecs/core/entities.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** 
+*/
+
+struct Position {
+    float x, y;
+};
+
+struct Velocity {
+    float x, y;
+};
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,20 +2,36 @@
 ** EPITECH PROJECT, 2023
 ** rtype
 ** File description:
-** main.cpp by thibb1
+** 
 */
 
-#include <iostream>
-#include <raylib.h>
-#include "Main.hpp"
+#include "ecs/core/entity_manager.hpp"
 
-int main()
-{
-    try {
-        rtype::Game game;
+int main() {
+    InitWindow(1280, 720, "Window");
 
-        game.createWindow();
-        game.run();
-    } catch (...) {}
+    EntityManager entities;
+    MovementSystem movement(entities);
+
+    int id = entities.create();
+    entities.setPosition(id, 0.0f, 0.0f);
+    entities.setVelocity(id, 1.0f, 2.0f);
+
+    float x = 10, y = 10;
+
+    while (!WindowShouldClose()) {
+        movement.update(GetFrameTime());
+        auto position = entities.getPosition(id);
+        auto velocity = entities.getVelocity(id);
+        x += velocity.x * GetFrameTime();
+        y += velocity.y * GetFrameTime();
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(TextFormat("Position: (%f, %f)", position.x, position.y), x, y, 20, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
+
     return 0;
 }
+
