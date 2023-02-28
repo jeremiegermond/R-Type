dd826490a92327c3853c0fcbe11c8c5c3050ee72
Thibb1 <thibault.brumaire@gmail.com>
2023-02-28 09:56:59 +0100

    [*] fix conan version

--- linux_build.sh
+++ linux_build.sh
@@ -2,12 +2,10 @@
 if ! command -v conan >> /dev/null
 then
     echo "First Build ? Installing conan using pip"
-    pip install conan
+    pip install conan==1.59.0
 fi
 
 mkdir -p bin
-#touch bin/server
-#touch bin/client
 mkdir -p build && cd build
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True --profile ../conanprofile_linux.txt
